cff755fbafe5dd3a9235ca5ee677009050e3c890
Niek Bebelaar <Niek.Bebelaar@vr-rr.nl>
2021-02-23 18:21:15 +0100

    [Database] Views voor alarmering en blusleiding

---
+++ database/views/blusleiding_view.sql
@@ -0,0 +1,10 @@
+create view bereikbaarheidskaart.blusleiding_view as
+select
+	lay.*
+from
+	bereikbaarheidskaart.blusleiding lay
+left join bereikbaarheidskaart.status c on
+	(ST_Within(lay.geom,
+	c.geom)) and status = 'concept'
+where
+	c.id is null
